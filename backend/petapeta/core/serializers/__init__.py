631c8289b799a15043fc26bcddd8f52d6381d27b
Najah7 <najahmatsuo7@gmail.com>
2023-07-09 03:46:04 +0900

    ✨ #28 [Add] シリアライザを作成

---
+++ backend/petapeta/core/serializers/__init__.py
@@ -0,0 +1,6 @@
+from core.serializers.user import UserSerializer
+from core.serializers.follow import FollowSerializer
+from core.serializers.post import PostSerializer
+from core.serializers.phote import PhoteSerializer
+from core.serializers.like import LikeSerializer
+from core.serializers.sticky_note import StickyNoteSerializer
