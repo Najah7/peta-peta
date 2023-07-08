e2a38cc51dcdea7e3b11c97bae5ab3424fd8934c
Najah7 <najahmatsuo7@gmail.com>
2023-07-09 07:17:07 +0900

    🐛 [Fix] モデルの修正（抜け要素の追加＆制約追加）

--- backend/petapeta/core/models/__init__.py
+++ backend/petapeta/core/models/__init__.py
@@ -1,14 +1,10 @@
-# UserとUserの中間テーブル
 from core.models.user import CustomUser
 
-# UserとUserの中間テーブル
 from core.models.follow import Follow
 
-# PostとPost関係のテーブル
 from core.models.post import Post
 from core.models.phote import Phote
-
-# UserとPostの中間テーブル
-from core.models.like import Like
 from core.models.sticky_note import StickyNote
 
+
+from core.models.like import PostLike, StickyNoteLike
