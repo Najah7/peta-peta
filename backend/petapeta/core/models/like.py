4958138b97fe344349ab05c89bdaebe7c8332ff2
Najah7 <najahmatsuo7@gmail.com>
2023-07-09 07:24:44 +0900

    🩹 [Fix] (再) __str__()の日本語の修正

--- backend/petapeta/core/models/like.py
+++ backend/petapeta/core/models/like.py
@@ -34,4 +34,4 @@ class StickyNoteLike(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self) -> str:
-        return self.user.name + 'がつけた'  + self.sticky_note.post.title + 'につけた付箋' 
+        return self.user.name + 'が'  + self.sticky_note.post.title + 'につけた付箋' 
