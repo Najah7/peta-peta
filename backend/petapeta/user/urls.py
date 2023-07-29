ccbc85ac9771331093ba094f40cf47b7460597de
Najah7 <najahmatsuo7@gmail.com>
2023-07-30 05:16:52 +0900

    🔥 userのurl.pyからfollowとunfollowを削除（違うアプリに分離したので）

--- backend/petapeta/user/urls.py
+++ backend/petapeta/user/urls.py
@@ -6,6 +6,4 @@ urlpatterns = [
     path('', views.ShowProfile.as_view(), name='show_profile'),
     path('create/', views.CreateUser.as_view(), name='create_user'),
     path('list/', views.UserList.as_view(), name='user_list'),
-    path('follow/<int:user_id>/', views.FollowUser.as_view(), name='follow_user'),
-    path('unfollow/<int:user_id>/', views.UnFollowUser.as_view(), name='unfollow_user'),
 ]
